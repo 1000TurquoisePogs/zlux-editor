0395a5443b56b55f09ae961bcb73917196841fdf
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-07-24 19:01:26 -0400

    Update datasets and rename file to buffer where appropriate
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- webClient/src/app/shared/model/editor-project.ts
+++ webClient/src/app/shared/model/editor-project.ts
@@ -21,7 +21,27 @@ export interface ProjectStructure {
     path?: string;
     fileName?: string;
     isDataset: boolean;
+    datasetAttrs?: DatasetAttributes;
     encoding?: number;
+}
+
+export interface DatasetAttributes {
+  volser: string;
+  recfm: RecordFormat;
+  dsorg: DatasetOrganization;
+}
+
+export interface RecordFormat {
+  carriageControl: string;
+  isBlocked: boolean;
+  recordLength: string;
+}
+
+export interface DatasetOrganization {
+  maxRecordLen: number;
+  organization: string;
+  totalBlockSize: number;
+  isPDSDir?: boolean;
 }
 
 /*
